636fea7f22f80306e46889a66e207820f8ec26f5
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-11-25 18:43:25 -0300

    init commit

---
+++ App.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace Wirefetch;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        Debug.WriteLine("Hello World!");
+    }
+
+    [STAThread]
+    public static void Main()
+    {
+        Application app = new();
+
+        app.Run(new MainWindow());
+    }
+}
