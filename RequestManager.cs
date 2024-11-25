be683ce2c7cc1b36c150b19840d3597ebd83ff68
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-11-25 19:03:05 -0300

    added standard fields for a http request (some may be missing)

--- RequestManager.cs
+++ RequestManager.cs
@@ -1,5 +1,16 @@
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Text;
+
 namespace Wirefetch;
 
 public class HttpRequestManager
 {
+	private HttpClient _httpClient;
+	private HttpMethod _method;
+	private string _endpointUrl;
+	private Dictionary<string, string> _headers;
+	private Dictionary<string, string> _queryParameters;
+	private HttpContent _body;
 }
