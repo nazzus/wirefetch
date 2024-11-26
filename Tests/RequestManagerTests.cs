c4c32d6d414158bfa9088fdc90c46eb774a6a372
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-11-25 22:26:36 -0300

    added get & post tests

--- Tests/RequestManagerTests.cs
+++ Tests/RequestManagerTests.cs
@@ -1,10 +1,45 @@
+using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
+using Wirefetch;
+
 namespace Tests;
 
-public class UnitTest1
+public class RequestManagerTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly HttpRequestManager _requestManager;
 
+    [Fact]
+    public async void GetRequest()
+    {
+        _requestManager.SetUri("https://postman-echo.com/get?hello=world");
+        HttpResponseMessage resp = await _requestManager.MakeRequest();
+
+        Debug.WriteLine(await resp.Content.ReadAsStringAsync());
+
+        Assert.Equal(HttpStatusCode.OK, resp.StatusCode);
+    }
+
+    [Fact]
+    public async void PostRequest()
+    {
+        var body = new
+        {
+            hello = "world"
+        };
+
+        _requestManager.SetUri("https://postman-echo.com/post");
+        _requestManager.SetMethod(HttpMethod.Post);
+        _requestManager.SetBody(JsonSerializer.Serialize(body));
+        HttpResponseMessage resp = await _requestManager.MakeRequest();
+
+        Debug.WriteLine(await resp.Content.ReadAsStringAsync());
+
+        Assert.Equal(HttpStatusCode.OK, resp.StatusCode);
+    }
+
+    public RequestManagerTests()
+    {
+        _requestManager = new HttpRequestManager();
     }
 }
