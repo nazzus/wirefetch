e30a829d43b986c80575d38b754d25c3d5c13a36
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-11-25 23:27:20 -0300

    init ui

--- src/App.cs
+++ src/App.cs
@@ -1,13 +1,34 @@
 ﻿using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Wirefetch;
 
 public partial class MainWindow : Window
 {
+    private readonly HttpRequestManager _requestManager;
+
     public MainWindow()
     {
-        Debug.WriteLine("Hello World!");
+        Title = "Wirefetch";
+        Width = 1280;
+        Height = 720;
+
+        _requestManager = new();
+
+        Loaded += new RoutedEventHandler(LoadUI);
+    }
+
+    private void LoadUI(object sender, RoutedEventArgs args)
+    {
+        TextBlock textBlock = new()
+        {
+            Text = "Hello world!",
+            VerticalAlignment = VerticalAlignment.Center,
+            HorizontalAlignment = HorizontalAlignment.Center
+        };
+
+        AddChild(textBlock);
     }
 
     [STAThread]
