03d02788538a60ed1bebf3a23d863840b6e14aaa
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-11-30 19:58:26 -0300

    added avalonia template & already working classes

---
+++ src/HttpRequestManager.cs
@@ -0,0 +1,117 @@
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace Wirefetch;
+
+public class HttpRequestManager
+{
+	// private fields
+	private readonly HttpClient _httpClient;
+	private HttpMethod _method;
+	private string _endpointUrl;
+	private readonly Dictionary<string, string> _headers;
+	private readonly Dictionary<string, string> _queryParameters;
+	private HttpContent _body;
+
+	// private methods
+	private static string ConvertToParameters(Dictionary<string, string> parameters)
+	{
+		var query = new StringBuilder();
+
+		if (parameters.Count == 0)
+			return string.Empty;
+
+		query.Append('?');
+
+		foreach (var param in parameters)
+		{
+			if (query.Length > 0)
+			{
+				query.Append('&');
+			}
+
+			query.Append(HttpUtility.UrlEncode(param.Key));
+			query.Append('=');
+			query.Append(HttpUtility.UrlEncode(param.Value));
+		}
+
+		return query.ToString();
+	}
+
+	// public methods
+	public void SetMethod(HttpMethod method)
+	{
+		_method = method;
+	}
+
+	public void SetUri(string url)
+	{
+		_endpointUrl = url;
+	}
+
+	public void SetBody(string body)
+	{
+		_body = new StringContent(body);
+	}
+
+	public void AddQueryParameter(string key, string value)
+	{
+		_queryParameters.Add(key, value);
+	}
+
+	public void AddHeader(string key, string value)
+	{
+		_queryParameters.Add(key, value);
+	}
+
+	public async Task<HttpResponseMessage> MakeRequest()
+	{
+		Uri fullUri = new(_endpointUrl + ConvertToParameters(_queryParameters));
+		HttpRequestMessage request = new(_method, fullUri)
+		{
+			Content = _body,
+		};
+
+		foreach (var header in _headers)
+		{
+			request.Headers.Add(header.Key, header.Value);
+		}
+
+		HttpResponseMessage response;
+
+		try
+		{
+			response = await _httpClient.SendAsync(request);
+		}
+
+		catch (HttpRequestException exception)
+		{
+			response = new HttpResponseMessage(exception.StatusCode ?? HttpStatusCode.NotFound)
+			{
+				ReasonPhrase = exception.Message
+			};
+		}
+
+		return response;
+	}
+
+
+	public HttpRequestManager()
+	{
+		_httpClient = new HttpClient();
+
+		_httpClient.DefaultRequestHeaders.Add("User-Agent", "Wirefetch/1.0");
+		_httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+		_method = HttpMethod.Get;
+		_headers = new Dictionary<string, string> { };
+		_body = new StringContent("");
+		_queryParameters = new Dictionary<string, string> { };
+		_endpointUrl = "";
+	}
+}
